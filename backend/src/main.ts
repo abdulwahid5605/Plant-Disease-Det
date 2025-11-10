2ad76c5d8fc3f0f00c1ce5b02cf1d6a27efff321
Abdul Wahid <abdulwahid4888888@gmail.com>
2025-11-10 13:52:38 -0800

    9 nov

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: http://localhost:3000`);
-
 }
 bootstrap();
